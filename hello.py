97121d286d5f03687d574124bc26a7b91f416c9d
Shuangtonglee <shuangtong_lee@163.com>
2018-12-04 20:21:10 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import time
+from selenium import webdriver
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>hello world!</h1>"
+
+@app.route('/test')
+def test():
+    driver=webdriver.PhantomJS(executable_path='D:/App/phantomjs/bin/phantomjs')
+    driver.get("https://www.baidu.com/")
+    time.sleep(3)
+    news = driver.find_element_by_class_name('mnav').text
+    return news
+
+
+if __name__ == "__main__":
+    app.run()
